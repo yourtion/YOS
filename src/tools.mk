fe842633a9fccd8753541565be463217c48e23d3
YourtionGuo <yourtion@gmail.com>
2016-06-22 12:13:42 +0800

    build and test for linux

--- src/tools.mk
+++ src/tools.mk
@@ -1,46 +1,48 @@
 UNAME := $(shell uname)
-ifeq ($(UNAME), Darwin)
-  MAKE     = make -r 
-  NASK     = $(TOOLPATH)nask
-  CC1      = $(TOOLPATH)gocc1 -I$(INCPATH) -Os -Wall -quiet
-  GAS2NASK = $(TOOLPATH)gas2nask -a
-  OBJ2BIM  = $(TOOLPATH)obj2bim
-  MAKEFONT = $(TOOLPATH)makefont
-  BIN2OBJ  = $(TOOLPATH)bin2obj
-  BIM2HRB  = $(TOOLPATH)bim2hrb
-  BIM2BIN  = $(TOOLPATH)bim2bin
-  EDIMG    = $(TOOLPATH)edimg
-  IMGTOL   = $(TOOLPATH)imgtol.com
-  GOLIB    = $(TOOLPATH)golib00 
-  COPY     = cp
-  DEL      = rm
-  TOOLPATH = $(TOOLSDIR)tools/osx/
-  INCPATH  = $(TOOLSDIR)tools/osx/haribote/
-  COPYSYS  = cat asmhead.bin bootpack.hrb > haribote.sys
+ifeq ($(OS),Windows_NT)
+	MAKE     = $(TOOLPATH)make.exe -r
+	NASK     = $(TOOLPATH)nask.exe
+	CC1      = $(TOOLPATH)cc1.exe -I$(INCPATH) -Os -Wall -quiet
+	GAS2NASK = $(TOOLPATH)gas2nask.exe -a
+	OBJ2BIM  = $(TOOLPATH)obj2bim.exe
+	MAKEFONT = $(TOOLPATH)makefont.exe
+	BIN2OBJ  = $(TOOLPATH)bin2obj.exe
+	BIM2HRB  = $(TOOLPATH)bim2hrb.exe
+	BIM2BIN  = $(TOOLPATH)bim2bin.exe
+	EDIMG    = $(TOOLPATH)edimg.exe
+	IMGTOL   = $(TOOLPATH)imgtol.com
+	GOLIB    = $(TOOLPATH)golib00.exe
+	COPY     = copy
+	DEL      = del
+	TOOLPATH = $(TOOLSDIR)tools/windows/
+	COPYSYS  = copy /B asmhead.bin+bootpack.hrb haribote.sys
 else
-  MAKE     = $(TOOLPATH)make.exe -r
-  NASK     = $(TOOLPATH)nask.exe
-  CC1      = $(TOOLPATH)cc1.exe -I$(INCPATH) -Os -Wall -quiet
-  GAS2NASK = $(TOOLPATH)gas2nask.exe -a
-  OBJ2BIM  = $(TOOLPATH)obj2bim.exe
-  MAKEFONT = $(TOOLPATH)makefont.exe
-  BIN2OBJ  = $(TOOLPATH)bin2obj.exe
-  BIM2HRB  = $(TOOLPATH)bim2hrb.exe
-  BIM2BIN  = $(TOOLPATH)bim2bin.exe
-  EDIMG    = $(TOOLPATH)edimg.exe
-  IMGTOL   = $(TOOLPATH)imgtol.com
-  GOLIB    = $(TOOLPATH)golib00.exe 
-  COPY     = copy
-  DEL      = del
-  TOOLPATH = $(TOOLSDIR)tools/windows/
-  INCPATH  = $(TOOLSDIR)tools/windows/haribote/
-  COPYSYS  = copy /B asmhead.bin+bootpack.hrb haribote.sys
+ifeq ($(UNAME), Darwin)
+	CC1      = $(TOOLPATH)gocc1 -I$(INCPATH) -Os -Wall -quiet 
+	TOOLPATH = $(TOOLSDIR)tools/osx/
+else
+	CC1      = $(TOOLPATH)cc1 -I $(INCPATH) -Os -Wall -quiet 
+	TOOLPATH = $(TOOLSDIR)tools/linux/
+endif
+	MAKE     = make -r  
+	NASK     = $(TOOLPATH)nask   
+	GAS2NASK = $(TOOLPATH)gas2nask -a  
+	OBJ2BIM  = $(TOOLPATH)obj2bim  
+	BIN2OBJ  = $(TOOLPATH)bin2obj  
+	BIM2HRB  = $(TOOLPATH)bim2hrb  
+	EDIMG    = $(TOOLPATH)edimg  
+	IMGTOL   = $(TOOLPATH)imgtol.com  
+	MAKEFONT = $(TOOLPATH)makefont  
+	GOLIB    = $(TOOLPATH)golib00  
+	COPY     = cp  
+	DEL      = rm  
+	COPYSYS  = cat asmhead.bin bootpack.hrb > haribote.sys
 endif
 
 LIBPATH   = $(ROOTPATH)libs/
 APILIBPATH = $(LIBPATH)apilib/
 STDLIBPATH = $(LIBPATH)stdlibs/
-INCPATH = $(ROOTPATH)includes/
+INCPATH  = $(ROOTPATH)includes/
 
 HARIBOTEPATH = $(ROOTPATH)kernel/
 TOOLSDIR = $(ROOTPATH)../
@@ -62,4 +64,4 @@ APPMKFILE = $(ROOTPATH)app.mk
 	$(BIM2HRB) $*.bim $*.org $(MALLOC)
 
 %.hrb : %.org Makefile
-	$(BIM2BIN) -osacmp in:$*.org out:$*.hrb
+	$(COPY) $(APP).org $(APP).hrb
